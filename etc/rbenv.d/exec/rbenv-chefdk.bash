0991512e12ca490a7912451e11b910b5c9540dcd
Mihai Petracovici <petracvv@users.noreply.github.com>
2019-06-17 16:14:26 -0600

    Use a different gem home for chef-workstation

--- etc/rbenv.d/exec/rbenv-chefdk.bash
+++ etc/rbenv.d/exec/rbenv-chefdk.bash
@@ -2,4 +2,6 @@
 
 if [[ "$RBENV_VERSION" = chef-workstation ]]; then
   eval "$(/opt/chef-workstation/bin/chef shell-init sh)"
+  export GEM_HOME=`echo "$GEM_HOME" | sed 's/chefdk/chefdk-workstation/'`
+  export GEM_PATH=`echo "$GEM_PATH" | sed 's/chefdk/chefdk-workstation/'`
 fi
