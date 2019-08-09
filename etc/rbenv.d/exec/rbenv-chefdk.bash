6f68231adaa48067ed09f942091caf7f3edae248
Mihai Petracovici <petracvv@users.noreply.github.com>
2019-08-09 09:34:47 -0600

    fix sed and PATH

--- etc/rbenv.d/exec/rbenv-chefdk.bash
+++ etc/rbenv.d/exec/rbenv-chefdk.bash
@@ -2,6 +2,7 @@
 
 if [[ "$RBENV_VERSION" = chef-workstation ]]; then
   eval "$(/opt/chef-workstation/bin/chef shell-init sh)"
-  export GEM_HOME=`echo "$GEM_HOME" | sed 's/chefdk/chefdk-workstation/'`
-  export GEM_PATH=`echo "$GEM_PATH" | sed 's/chefdk/chefdk-workstation/'`
+  export PATH=`echo "$GEM_HOME" | /usr/bin/sed 's/chefdk/chefdk-workstation/'`
+  export GEM_HOME=`echo "$GEM_HOME" | /usr/bin/sed 's/chefdk/chefdk-workstation/'`
+  export GEM_PATH=`echo "$GEM_PATH" | /usr/bin/sed 's/chefdk/chefdk-workstation/'`
 fi
